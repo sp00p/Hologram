f4dde47f1f3fa37e8c2bbff59ccb782c24e052a7
sp00p <46919543+sp00p@users.noreply.github.com>
2020-06-22 17:55:39 -0700

    Optimize code
    Removed the god awful switch statement and created a simple function to convert &'s to ChatColor so you can now use &'s instead of color's. This also means that colors are no longer supported. The new syntax can be found in the commands section of the wiki. You can also add spaces using _'s in your messages. Future updates will include the creation of multi-line holograms and a better delete system.

--- src/main/java/org/sean/hologram/HologramListener.java
+++ src/main/java/org/sean/hologram/HologramListener.java
@@ -13,8 +13,12 @@ import static org.bukkit.Bukkit.getServer;
 
 public class HologramListener implements CommandExecutor, Listener {
 
-    // hologram create c [COLOR] y [y position] <bold> <itallic> <strikethrough> <magic> <message>
-    //            0    1    2    3       4        5        6           7            8       9
+    // hologram create cc <msg> y [y position] (spaces with _)
+    //            0     1   2   3       4
+
+    public String color (String string) {
+        return ChatColor.translateAlternateColorCodes('&', string);
+    }
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
@@ -24,129 +28,29 @@ public class HologramListener implements CommandExecutor, Listener {
 
             if (cmd.getName().equalsIgnoreCase("hologram")) {
                 if (args[0].equals("create")) {
-
-                    String message;
-
-                    if (args[1].equalsIgnoreCase("c") && args[3].equals("y")) {
-
+                    
+                    if (args[1].equalsIgnoreCase("cc") && args[3].equalsIgnoreCase("y")) {
+                        // hologram create cc <msg> y yNum (note: spaces with _)
+                        String string = args[2];
                         double yNum = Double.parseDouble(args[4]);
-                        ChatColor setColor;
-                        String color = args[2];
 
-                        switch (color) {
-                            case "AQUA":
-                                setColor = ChatColor.AQUA;
-                                break;
-                            case "BLACK":
-                                setColor = ChatColor.BLACK;
-                                break;
-                            case "BLUE":
-                                setColor = ChatColor.BLUE;
-                                break;
-                            case "DARK_AQUA":
-                                setColor = ChatColor.DARK_AQUA;
-                                break;
-                            case "DARK_BLUE":
-                                setColor = ChatColor.DARK_BLUE;
-                                break;
-                            case "DARK_GRAY":
-                                setColor = ChatColor.DARK_GRAY;
-                                break;
-                            case "DARK_GREEN":
-                                setColor = ChatColor.DARK_GREEN;
-                                break;
-                            case "DARK_PURPLE":
-                                setColor = ChatColor.DARK_PURPLE;
-                                break;
-                            case "DARK_RED":
-                                setColor = ChatColor.DARK_RED;
-                                break;
-                            case "GOLD":
-                                setColor = ChatColor.GOLD;
-                                break;
-                            case "GRAY":
-                                setColor = ChatColor.GRAY;
-                                break;
-                            case "GREEN":
-                                setColor = ChatColor.GREEN;
-                                break;
-                            case "LIGHT_PURPLE":
-                                setColor = ChatColor.LIGHT_PURPLE;
-                                break;
-                            case "RED":
-                                setColor = ChatColor.RED;
-                                break;
-                            case "YELLOW":
-                                setColor = ChatColor.YELLOW;
-                                break;
-                            default:
-                                plr.sendMessage(ChatColor.RED + "Please enter a color!");
-                                plr.sendMessage(ChatColor.GRAY + "Color argument expected color but you gave it " + color);
-                                setColor = ChatColor.WHITE;
-                                break;
-                        }
+                        if (string.contains("&")) {
 
+                            String editedString = color(string);
+                            String finalmsg = editedString.replace("_"," ").trim();
 
-                        ArmorStand stand = (ArmorStand) plr.getWorld().spawnEntity(plr.getLocation().add(0, yNum, 0), EntityType.ARMOR_STAND);
-                        stand.setVisible(false);
-                        stand.setGravity(false);
-                        stand.setInvulnerable(true);
+                            ArmorStand stand = (ArmorStand) plr.getWorld().spawnEntity(plr.getLocation().add(0, yNum, 0), EntityType.ARMOR_STAND);
+                            stand.setVisible(false);
+                            stand.setGravity(false);
+                            stand.setInvulnerable(true);
 
-                        stand.setCustomNameVisible(true);
-                        message = args[9];
-
-
-                        if (args[5].equalsIgnoreCase("true") && args[6].equalsIgnoreCase("false") && args[7].equalsIgnoreCase("false") && args[8].equalsIgnoreCase("false")) { // t f f f
-                            stand.setCustomName(setColor + "" + ChatColor.BOLD + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("false") && args[6].equalsIgnoreCase("true") && args[7].equalsIgnoreCase("false") && args[8].equalsIgnoreCase("false")) { // f t f f
-                            stand.setCustomName(setColor + "" + ChatColor.ITALIC + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("false") && args[6].equalsIgnoreCase("false") && args[7].equalsIgnoreCase("true") && args[8].equalsIgnoreCase("false")) { // f f t f
-                            stand.setCustomName(setColor + "" + ChatColor.STRIKETHROUGH + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("false") && args[6].equalsIgnoreCase("false") && args[7].equalsIgnoreCase("false") && args[8].equalsIgnoreCase("true")) { // f f f t
-                            stand.setCustomName(setColor + "" + ChatColor.MAGIC + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("true") && args[6].equalsIgnoreCase("true") && args[7].equalsIgnoreCase("false") && args[8].equalsIgnoreCase("true")) { // t t f t
-                            stand.setCustomName(setColor + "" + ChatColor.BOLD + "" + ChatColor.ITALIC + "" + ChatColor.MAGIC + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("false") && args[6].equalsIgnoreCase("true") && args[7].equalsIgnoreCase("true") && args[8].equalsIgnoreCase("false")) { // f t t f
-                            stand.setCustomName(setColor + "" + ChatColor.ITALIC + "" + ChatColor.STRIKETHROUGH + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("false") && args[6].equalsIgnoreCase("false") && args[7].equalsIgnoreCase("true") && args[8].equalsIgnoreCase("true")) { // f f t t
-                            stand.setCustomName(setColor + "" + ChatColor.STRIKETHROUGH + "" + ChatColor.MAGIC + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("true") && args[6].equalsIgnoreCase("true") && args[7].equalsIgnoreCase("true") && args[8].equalsIgnoreCase("false")) { // t t t f
-                            stand.setCustomName(setColor + "" + ChatColor.BOLD + "" + ChatColor.ITALIC + "" + ChatColor.STRIKETHROUGH + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("true") && args[6].equalsIgnoreCase("false") && args[7].equalsIgnoreCase("true") && args[8].equalsIgnoreCase("true")) { // t f t t
-                            stand.setCustomName(setColor + "" + ChatColor.BOLD + "" + ChatColor.STRIKETHROUGH + "" + ChatColor.MAGIC + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("false") && args[6].equalsIgnoreCase("true") && args[7].equalsIgnoreCase("false") && args[8].equalsIgnoreCase("true")) { // f t f t
-                            stand.setCustomName(setColor + "" + ChatColor.ITALIC + "" + ChatColor.MAGIC + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("false") && args[6].equalsIgnoreCase("true") && args[7].equalsIgnoreCase("true") && args[8].equalsIgnoreCase("true")) { // f t t t
-                            stand.setCustomName(setColor + "" + ChatColor.ITALIC + "" + ChatColor.STRIKETHROUGH + "" + ChatColor.MAGIC + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("true") && args[6].equalsIgnoreCase("false") && args[7].equalsIgnoreCase("false") && args[8].equalsIgnoreCase("true")) { // t f f t
-                            stand.setCustomName(setColor + "" + ChatColor.BOLD + "" + ChatColor.MAGIC + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("true") && args[6].equalsIgnoreCase("false") && args[7].equalsIgnoreCase("true") && args[8].equalsIgnoreCase("false")) { // t f t f
-                            stand.setCustomName(setColor + "" + ChatColor.BOLD + "" + ChatColor.STRIKETHROUGH + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("true") && args[6].equalsIgnoreCase("true") && args[7].equalsIgnoreCase("true") && args[8].equalsIgnoreCase("true")) { // t t t t
-                            stand.setCustomName(setColor + "" + ChatColor.BOLD + "" + ChatColor.ITALIC + "" + ChatColor.STRIKETHROUGH + "" + ChatColor.MAGIC + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("true") && args[6].equalsIgnoreCase("true") && args[7].equalsIgnoreCase("false") && args[8].equalsIgnoreCase("false")) { // t t f f
-                            stand.setCustomName(setColor + "" + ChatColor.BOLD + "" + ChatColor.ITALIC + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
-                        } else if (args[5].equalsIgnoreCase("false") && args[6].equalsIgnoreCase("false") && args[7].equalsIgnoreCase("false") && args[8].equalsIgnoreCase("false")) { // f f f f
-                            stand.setCustomName(setColor + message);
-                            plr.sendMessage(ChatColor.GREEN + "Sucessfully created hologram with name " + message);
+                            stand.setCustomNameVisible(true);
+                            stand.setCustomName(finalmsg);
                         } else {
-                            plr.sendMessage(ChatColor.RED + "Incorrect formatting!");
+                            plr.sendMessage(ChatColor.RED + "String " + string + " does not contain &");
                         }
+
+
                     }
                 } else if(args[0].equals("delete")) {
                     try {
@@ -163,7 +67,3 @@ public class HologramListener implements CommandExecutor, Listener {
         return false;
     }
 }
-
-
-
-
